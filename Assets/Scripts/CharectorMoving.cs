7db8dbf0af883f91eb084dc145f3e1edc0d897b5
Ildar <mylashev@gmail.com>
2021-05-30 14:17:15 +0300

    Signaling

---
+++ Assets/Scripts/CharectorMoving.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharectorMoving : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+    private Transform _transform;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private bool _isGrounded;
+
+    private void Start()
+    {
+        _transform = gameObject.GetComponent<Transform>();
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        _animator = gameObject.GetComponent<Animator>();
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _spriteRenderer.flipX = true;
+            Move(-_speed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _spriteRenderer.flipX = false;
+            Move(_speed);
+        }
+        else
+        {
+            _animator.SetTrigger("Idle");
+        }
+
+        if (_isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _isGrounded = false;
+    }
+
+    private void Move(float MoveDirection)
+    {
+        _transform.Translate(MoveDirection * Time.deltaTime, 0, 0);
+        _animator.SetTrigger("Run");
+    }
+
+    private void Jump()
+    {
+        _rigidbody2D.AddForce(Vector2.up * _jumpForce);
+    }
+}
---
+++ Assets/Scripts/EnemyAlarm.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAlarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] [Range(0f, 1f)] private float _maxVolume;
+    [SerializeField] [Range(0f, 0.1f)] private float _runTime;
+
+    private bool _isPlayerEnter;
+    private Animator _enemyAnimator;
+    private GameObject _alarmBox;
+
+    void Start()
+    {
+        _isPlayerEnter = false;
+        _enemyAnimator = gameObject.GetComponent<Animator>();
+        _alarmBox = GameObject.FindGameObjectWithTag("AlarmBox");
+    }
+
+    void Update()
+    {
+        if (_isPlayerEnter)
+        {
+            PlayAlarm();
+        }
+        else
+        {
+            StopAlarm();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _isPlayerEnter = true;
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isPlayerEnter = false;
+    }
+
+    private void PlayAlarm()
+    {
+        IsPlaying(_audioSource.isPlaying);
+
+        if (_audioSource.volume < 0.5f)
+        {
+            _audioSource.volume += ChangeVolume();
+        }
+
+        _enemyAnimator.SetTrigger("Alarm");
+        _alarmBox.active = true;
+    }
+
+    private void StopAlarm()
+    {
+        if (_audioSource.volume > 0f)
+        {
+            IsPlaying(_audioSource.isPlaying);
+            _audioSource.volume -= ChangeVolume();
+        }
+        else
+        {
+            _audioSource.Stop();
+            _enemyAnimator.SetTrigger("Idle");
+            _alarmBox.active = false;
+        }
+    }
+
+    private void IsPlaying(bool isPlay)
+    {
+        if (isPlay == false)
+        {
+            _audioSource.Play();
+        }
+    }
+
+    private float ChangeVolume()
+    {
+        return Mathf.MoveTowards(0, _maxVolume, _runTime * Time.deltaTime);
+    }
+}
