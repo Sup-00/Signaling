bebfaa074d9c20f2fba85229542aaec1d331fcaa
Ildar <mylashev@gmail.com>
2021-06-08 19:20:12 +0300

    Sup_0

--- Assets/Scripts/CharectorMoving.cs
+++ Assets/Scripts/CharectorMoving.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody2D))] 
 public class CharectorMoving : MonoBehaviour
 {
     [SerializeField] private float _speed;
